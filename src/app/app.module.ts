77150e33c5fcad8a1d09b8722e2cc8e422a59d0c
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-06-12 12:03:25 +0200

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 import { AppComponent } from './app.component';
+
+// Cambiar el locale de la app de forma global
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    PrimeNgModule,
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
