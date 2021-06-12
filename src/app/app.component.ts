4570fa2749b00c104ce231155fa4f6e5c39c1b07
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-06-12 12:03:25 +0200

    pipes app final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipesApp';
+export class AppComponent implements OnInit {
+  
+  nombre: string = 'gatito';
+  valor: number = 1000;
+  obj: {a:string, b:string} = {a:'asd', b:'jkl'};
+
+  constructor(private primengConfig: PrimeNGConfig) {
+
+  }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+    
+    
+  }
+
 }
